58d750c98de82ab8588910477b91d948bba47386
unknown <dkfkgks64@gmail.com>
2025-11-10 22:47:10 +0900

    feature: 2回送信するように変更（2回目の受信は失敗）

--- lib/spawn.ex
+++ lib/spawn.ex
@@ -1,5 +1,27 @@
 defmodule Spawn do
   def greet do
-    IO.puts "Hello"
+    # この関数は繰り返し処理をしていない。
+    receive do
+      {sender, msg} ->
+        send sender, {:ok, "初めまして！#{msg}さん。"}
+    end
   end
+end
+
+pid = spawn(Spawn, :greet, [])
+
+# 1回目の送信（受信成功）
+send pid, {self(), "かなえ"}
+
+receive do
+  {:ok, msg} ->
+    IO.puts msg
+end
+
+# 2回目の送信（待ち続ける）
+send pid, {self(), "滝口"}
+
+receive do
+  {:ok, msg} ->
+    IO.puts msg
 end
