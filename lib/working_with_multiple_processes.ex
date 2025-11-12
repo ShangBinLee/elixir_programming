d4081cfcf43cef8201f84c447c40b7177c77a6be
unknown <dkfkgks64@gmail.com>
2025-11-13 02:14:41 +0900

    feature: WorkingWithMultipleProcesses-3の解答作成

--- lib/working_with_multiple_processes.ex
+++ lib/working_with_multiple_processes.ex
@@ -31,4 +31,56 @@ defmodule WorkingWithMultipleProcesses do
       send betty_p, fred_p
     end
   end
+
+  defmodule Problem3 do
+    @moduledoc """
+    # 練習問題：WorkingWithMultipleProcesses-3
+
+    spawn_linkを使ってプロセスを生成し、そのプロセスで親プロセスへメッセージを送信した後、\s\s
+    すぐに終了するようにしてみよう。その間、親プロセスでは500ミリ秒スリープし、その後で、\s\s
+    受信を待機しているメッセージをすべて受信するようにしよう。何を受信するか、調査してほしい。\s\s
+    子プロセスが終了するとき、子プロセスからの通知を待っていないことが問題になるだろうか？
+    """
+    import :timer, only: [sleep: 1]
+
+    def child_process(send_to, sleep_time) do
+      send send_to, "子プロセスからのメッセージです。"
+      if sleep_time > 0, do: sleep sleep_time
+      exit(:boom)
+    end
+
+
+    @doc """
+    親プロセス。
+
+    sleep_timeを
+    1. 0（子プロセスがすぐ終了）
+    1. 500（親プロセスのsleep時間と同じ）
+    3. 500より大きい数
+
+    に指定して確認してみよう。
+
+    ## パラメータ
+
+      - sleep_time：子プロセスに指定するsleep時間（ミリ秒）
+
+    ## 例
+
+        iex>WorkingWithMultipleProcesses.Problem3.run(0)
+        # メッセージ受信前にプロセス異常終了
+        iex>WorkingWithMultipleProcesses.Problem3.run(500)
+        # 500ミリ秒sleepし、メッセージ受信前にプロセス異常終了
+        iex>WorkingWithMultipleProcesses.Problem3.run(600)
+        # 600ミリ秒sleepし、メッセージ受信後、プロセス異常終了
+    """
+    def run(sleep_time) do
+      spawn_link(WorkingWithMultipleProcesses.Problem3, :child_process, [self(), sleep_time])
+      sleep 500
+
+      receive do
+        msg ->
+          "メッセージ受信：#{msg}"
+      end
+    end
+  end
 end
