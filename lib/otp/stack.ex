c609ba8b58e342e35d1c580166767421bdc18c63
unknown <dkfkgks64@gmail.com>
2025-11-22 00:23:03 +0900

    feature: OTP-Servers-2の解答作成

--- lib/otp/stack.ex
+++ lib/otp/stack.ex
@@ -9,6 +9,13 @@ defmodule Otp.Stack do
   例えば、もし `[5, "cat", 9]`で初期化したのなら、連続したpopの呼び出しは、`5`、\s\s
   `"cat"`そして`9`を返す。
 
+  # 練習問題：OTP-Servers-2
+
+  この前の練習問題で作ったスタックサーバに、\s\s
+  値をスタックのトップに追加するpushインターフェースを追加しよう。\s\s
+  castを使って実装できる。\s\s
+  IExで、値をプッシュしたりポップしたりしてみよう。
+
   """
 
   use GenServer
@@ -23,5 +30,9 @@ defmodule Otp.Stack do
 
   def handle_call(:pop, _from, []) do
     {:reply, {:error, "空のスタックです。"}, []}
+  end
+
+  def handle_cast({:push, el}, stack) do
+    {:noreply, [el | stack]}
   end
 end
