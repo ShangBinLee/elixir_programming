882ae364867d9cb3b563774b3f9c74f1da54ab5e
unknown <dkfkgks64@gmail.com>
2025-11-30 02:10:05 +0900

    feature: LinkingModules-BehavioursAndUse-2の解答作成

--- lib/use/tracer.ex
+++ lib/use/tracer.ex
@@ -1,4 +1,6 @@
 defmodule Use.Tracer do
+  import IO.ANSI
+
   def dump_args(args) do
     args |> Enum.map(&inspect/1) |> Enum.join(", ")
   end
@@ -7,15 +9,30 @@ defmodule Use.Tracer do
     "#{name}(#{dump_args(args)})"
   end
 
+  @doc """
+  # 練習問題：LinkingModules-BehavioursAndUse-2
+
+  組込み関数の`IO.ANSI`はANSIエスケープシーケンスを表現する関数を定義する。\s\s
+  これを使って、（例えば）文字列をターミナルに出力する際に、色や太字、反転、アンダーライン\s\s
+  といった効果をつけることができる（ターミナルがサポートしていれば。）\s\s
+
+        iex> import IO.ANSI
+        iex> IO.puts ["Hello", white(), green_background(), "world!"]
+        Hello, World!
+
+  モジュールを調査し、それを使って、トレースの出力を色付けしてみよう。\s\s
+  文字列のリストを`IO.puts`に渡すと、なぜ動くのだろうか。
+
+  """
   defmacro def(definition = {name, _, args}, do: content) do
     quote do
       Kernel.def(unquote(definition)) do
         # unquote(args)はdefで定義した関数のパラメータ名のリストにパースされる
         # つまり、マクロ関数に渡された時は内部表現であったものがunquoteでパラメータのリストになる。
         # それで、関数の呼び出し時に渡される引数をパラメータ名で参照できるようになる。
-        IO.puts "===> call: #{dump_defn(unquote(name), unquote(args))}"
+        IO.puts [blue(), "===> call: #{dump_defn(unquote(name), unquote(args))}", default_color()]
         result = unquote(content)
-        IO.puts "===> result: #{result}"
+        IO.puts [blue(), "===> result: #{result}", default_color()]
         result
       end
     end
