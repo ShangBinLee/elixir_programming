28d9548bf26818eba553d521ea64ba8e89b7e5f9
unknown <dkfkgks64@gmail.com>
2025-12-05 02:14:34 +0900

    feature: Inspect.Protocols.Midi、Inspect.Protocols.Midi.Frame
    プロトコル実装.

--- lib/protocols/midi.ex
+++ lib/protocols/midi.ex
@@ -80,3 +80,48 @@ defimpl Collectable, for: Protocols.Midi do
     }
   end
 end
+
+defimpl Inspect, for: Protocols.Midi do
+  def inspect(%Protocols.Midi{content: <<>>}, _opts) do
+    "#Midi[<<empty>>]"
+  end
+
+  def inspect(midi = %Protocols.Midi{}, _opts) do
+    content =
+      Enum.map(midi, fn frame -> inspect frame end)
+      |> Enum.join("\n")
+    "#Midi[\n#{content}\n]"
+  end
+end
+
+defimpl Inspect, for: Protocols.Midi.Frame do
+  def inspect(%Protocols.Midi.Frame{
+    type: "MThd",
+    length: 6,
+    data: <<
+      format::integer-16,
+      tracks::integer-16,
+      division::bits-16
+    >>},
+    _opts) do
+    beats = decode(division)
+    "#Midi.Header{Midi format: #{format}, tracks: #{tracks}, timing: #{beats}}"
+  end
+
+  def inspect(%Protocols.Midi.Frame{
+    type: "MTrk",
+    length: length,
+    data: data
+  },
+  _opts) do
+    "#Midi.Track{length: #{length}, data: #{inspect data}}"
+  end
+
+  def decode(<<0::1, beats::15>>) do
+    "♪ = #{beats}"
+  end
+
+  def decode(<<1::1, fps::7, beats::8>>) do
+    "#{-fps} fps, #{beats}/frame"
+  end
+end
