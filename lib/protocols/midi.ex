f0a85a5fe88fba6261c1bf73fa2ebfc19c03dd05
unknown <dkfkgks64@gmail.com>
2025-12-05 00:37:43 +0900

    feature: Protocols.Midi、Protocols.Midi.Frameモジュール作成

---
+++ lib/protocols/midi.ex
@@ -0,0 +1,23 @@
+defmodule Protocols.Midi do
+  defstruct(content: <<>>)
+
+  defmodule Frame do
+    defstruct(
+      type: "xxxx",
+      length: 0,
+      data: <<>>
+    )
+
+    def to_binary(%__MODULE__{type: type, length: length, data: data}) do
+      <<
+        type::binary-4,
+        length::integer-32,
+        data::binary
+      >>
+    end
+  end
+
+  def from_file(name) do
+    %__MODULE__{content: File.read!(name)}
+  end
+end
